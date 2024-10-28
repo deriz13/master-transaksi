9978fe8005fb4832d043f962af9adef30dd331e3
Deriz13 <dederizki130102@gmail.com>
2024-10-28 19:38:21 +0700

    fix template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MasterCategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('/', MasterCategoryController::class);
